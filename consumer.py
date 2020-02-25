5da30e742f6ce28cc27bba9fb7fc332c019aafb7
quyixiao <2621048238@qq.com>
2020-02-25 16:30:38 +0800

    提交修改

--- consumer.py
+++ consumer.py
@@ -5,6 +5,9 @@ connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='172.16.157.242', port=5672, virtual_host='/', credentials=credentials))
 channel = connection.channel()
 # 申明消息队列，消息在这个队列传递，如果不存在，则创建队列
+# 声明exchange，由exchange指定消息在哪个队列传递，如不存在，则创建.durable = True 代表exchange持久化存储，False 非持久化存储
+# 注意：如果已存在一个非持久化的 queue 或 exchange ，执行上述代码会报错，因为当前状态不能更改 queue 或 exchange 存储属性，
+# 需要删除重建。如果 queue 和 exchange 中一个声明了持久化，另一个没有声明持久化，则不允许绑定。
 channel.queue_declare(queue='python-test33', durable=False)
 
 
--- product.py
+++ product.py
@@ -13,6 +13,8 @@ result = channel.queue_declare(queue='python-test')
 for i in range(10):
     message = json.dumps({'OrderId': "1000%s" % i})
     # 向队列插入数值 routing_key是队列名
-    channel.basic_publish(exchange='', routing_key='python-test', body=message)
+    # 向队列插入数值 routing_key是队列名。delivery_mode = 2 声明消息在队列中持久化，delivery_mod = 1 消息非持久化
+    channel.basic_publish(exchange='', routing_key='python-test', body=message,
+                          properties=pika.BasicProperties(delivery_mode=2))
     print(message)
 connection.close()
